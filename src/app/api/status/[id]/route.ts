4c3ba75841deefb866bb78646ab0778c7c2367ab
Debmalya Sen <debmalyasen37@gmail.com>
2025-01-28 16:10:25 +0530

    #udate in status

--- src/app/api/status/[id]/route.ts
+++ src/app/api/status/[id]/route.ts
@@ -1,50 +1,53 @@
+import crypto from "crypto";
 import { NextRequest, NextResponse } from "next/server";
 import sha256 from "crypto-js/sha256";
 import axios from "axios";
+import { headers } from "next/headers";
 
-export async function POST(req:Request, res:Response) {
-  const data = await req.formData();
+
+export async function POST(req:Request) {
+  try {
+    const data = await req.formData();
   console.log(data);
   const status = data.get("code");
   const merchantId = data.get("merchantId");
   const transactionId = data.get("transactionId");
 
-  const st =
-    `/pg/v1/status/${merchantId}/${transactionId}` +
-    process.env.PHONEPE_SALT_KEY;
-  // console.log(st)
-  const dataSha256 = sha256(st);
+    const keyIndex = 1;
 
-  const checksum = dataSha256 + "###" + process.env.PHONEPE_SALT_INDEX;
-  console.log(checksum);
+    const string =
+      `/pg/v1/status/${merchantId}/${transactionId}` + process.env.PHONEPE_SALT_KEY;
+    const sha256 = crypto.createHash("sha256").update(string).digest("hex");
+    const checksum = sha256 + "###" + keyIndex;
 
+    const options = {
+      method: "GET",
+      url: `https://api.phonepe.com/apis/hermes/pg/v1/status/${merchantId}/${transactionId}`,
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+        "X-VERIFY": checksum,
+        "X-MERCHANT-ID": `${merchantId}`,
+      },
+    };
 
+    const response = await axios(options);
 
-
-  const options = {
-    method: "GET",
-    url: `https://api.phonepe.com/apis/hermes/pg/v1/status/${merchantId}/${transactionId}`,
-    headers: {
-      accept: "application/json",
-      "Content-Type": "application/json",
-      "X-VERIFY": checksum,
-      "X-MERCHANT-ID": `${merchantId}`,
-    },
-  };
-
-  // CHECK PAYMENT STATUS
-  const response = await axios.request(options);
-  console.log("r===", response.data.code);
-
-
-  if (response.data.code == "PAYMENT_SUCCESS")
-  return NextResponse.redirect("https://hiuri.in/success",{
-    status: 301,
-  });
-else return NextResponse.redirect("https://hiuri.in/failure",{
-  // a 301 status is required to redirect from a POST to a GET route
-  status: 301,
-});
-
-
+    if (response.data.success === true) {
+      return NextResponse.redirect("https://localhost:3000/success", {
+        status: 301,
+      });
+    } else {
+      return NextResponse.redirect("https://localhost:3000/failure", {
+        status: 301,
+      });
+    }
+  } catch (error:any) {
+    console.error(error);
+    // Return error response
+    return NextResponse.json(
+      { error: "Payment check failed", details: error.message },
+      { status: 500 }
+    );
+  }
 }
